13c06d43f4afc7104713857fe642778b508d8a12
KahungLee <kahunglee@qq.com>
2018-12-19 13:03:45 +0800

    Update general.c

--- src/rules/general.c
+++ src/rules/general.c
@@ -40,9 +40,9 @@ int general_seccomp_rules(struct config *_config) {
 //     if (seccomp_rule_add(ctx, SCMP_ACT_KILL, SCMP_SYS(open), 1, SCMP_CMP(1, SCMP_CMP_MASKED_EQ, O_WRONLY, O_WRONLY)) != 0) {
 //         return LOAD_SECCOMP_FAILED;
 //     }
-//     if (seccomp_rule_add(ctx, SCMP_ACT_KILL, SCMP_SYS(open), 1, SCMP_CMP(1, SCMP_CMP_MASKED_EQ, O_RDWR, O_RDWR)) != 0) {
-//         return LOAD_SECCOMP_FAILED;
-//     }
+    if (seccomp_rule_add(ctx, SCMP_ACT_KILL, SCMP_SYS(open), 1, SCMP_CMP(1, SCMP_CMP_MASKED_EQ, O_RDWR, O_RDWR)) != 0) {
+        return LOAD_SECCOMP_FAILED;
+    }
     // do not allow "w" and "rw" using openat
     if (seccomp_rule_add(ctx, SCMP_ACT_KILL, SCMP_SYS(openat), 1, SCMP_CMP(2, SCMP_CMP_MASKED_EQ, O_WRONLY, O_WRONLY)) != 0) {
         return LOAD_SECCOMP_FAILED;
